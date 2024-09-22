2a4bb34ab8ecd10ecef9a324298cc2d69a126f94
xenosenim <169744738+xenosenim@users.noreply.github.com>
2024-09-22 18:47:30 +0300

    22.09.2024 18:47

--- zones/bd-dhamma/text-refactor.sh
+++ zones/bd-dhamma/text-refactor.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #-------------------------------------------------------------------------------
 
-dir_prefix="${HOME}/mnt/data/Webpages/zones/de-grammatik"
+dir_prefix="${HOME}/mnt/data/Webpages/zones/gm-reference"
 dir_sounds="${dir_prefix}/sounds"
 dir_sources="${dir_prefix}/sources"
 dir_texts="${dir_prefix}/texts"
 
-number_start=19
+number_start=11
 number_shift=1
 
 #-------------------------------------------------------------------------------
