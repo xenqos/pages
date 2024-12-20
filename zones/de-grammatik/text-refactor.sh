02289b71b77f55a6ebbc2ee9b61fe406a720f266
xenqos <169744738+xenqos@users.noreply.github.com>
2024-12-20 11:22:49 +0200

    12-20-2024 11:22

--- zones/de-grammatik/text-refactor.sh
+++ zones/de-grammatik/text-refactor.sh
@@ -6,8 +6,8 @@ dir_sounds="${dir_prefix}/sounds"
 dir_sources="${dir_prefix}/sources"
 dir_texts="${dir_prefix}/texts"
 
-number_start=50
-number_shift=2
+number_start=55
+number_shift=1
 
 #
 exit 0
