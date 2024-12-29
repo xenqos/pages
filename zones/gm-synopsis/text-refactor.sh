e9e4e48d919905ae68cf6b41b6877a2b427b0126
xenqos <169744738+xenqos@users.noreply.github.com>
2024-12-29 20:59:08 +0200

    12-29-2024 20:59

--- zones/gm-synopsis/text-refactor.sh
+++ zones/gm-synopsis/text-refactor.sh
@@ -6,10 +6,11 @@ dir_sounds="${dir_prefix}/sounds"
 dir_sources="${dir_prefix}/sources"
 dir_texts="${dir_prefix}/texts"
 
-number_start=25
+number_start=26
 number_shift=1
 
-#exit 0
+#
+exit 0
 
 #-------------------------------------------------------------------------------
 
