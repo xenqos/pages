be66d7e725b85186907ebe1ba23ea7f486753d77
xenqos <169744738+xenqos@users.noreply.github.com>
2026-03-07 21:07:20 +0200

    07.03.26 21:07

---
+++ zones/electricity/direct-current/images/get-images-01-DC.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# -v verbose
+# -s silent
+# -H header
+# -A user agent
+
+user_agent='Mozilla/5.0 (X11; Linux x86_64; rv:147.0; ffff) Gecko/20100101 Firefox/147.0'
+
+prefix='https://www.allaboutcircuits.com'
+
+# sleep 2; echo '002'; curl -s -A "${user_agent}" "${prefix}/textbook/direct-current/chpt-1/static-electricity/"
+
+#sleep 2; echo '002'; wget "https://sub.allaboutcircuits.com/images/10008.png"
+#sleep 2; echo '002'; wget "https://sub.allaboutcircuits.com/images/10009.png"
+#sleep 2; echo '002'; wget "https://sub.allaboutcircuits.com/images/10010.png"
+#sleep 2; echo '002'; wget "https://sub.allaboutcircuits.com/images/10011.png"
+#sleep 2; echo '002'; wget "https://sub.allaboutcircuits.com/images/10012.png"
+#sleep 2; echo '002'; wget "https://sub.allaboutcircuits.com/images/10013.png"
+sleep 2; echo '002'; wget "https://sub.allaboutcircuits.com/images/10014.png"
